c64239331b384c06d08970aed7c9c5acc0e5ee3e
Kushagra Singh <kushthaksh@gmail.com>
2021-05-28 11:44:10 -0700

    Better UI

---
+++ code.js
@@ -0,0 +1,19 @@
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header").style.height = "80%";
+    document.getElementById("header").style.width = "90%";
+    document.getElementById("header").style.marginLeft = "5%";
+    document.getElementById("header").style.marginRight = "1%";
+    document.getElementById("header").style.borderRadius = "1%";
+  } else {
+    document.getElementById("header").style.height = "100%";
+    document.getElementById("header").style.width = "100%";
+    
+    document.getElementById("header").style.marginLeft = "0";
+    document.getElementById("header").style.marginRight = "0";
+    document.getElementById("header").style.borderRadius = "0";
+  }
+} 
